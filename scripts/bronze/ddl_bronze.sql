c1879a2b2a4c3d0365cf21c8f7689c364b7d4628
Cosmin Ion <ion.cosmin98@outlook.com>
2026-03-31 13:42:26 +0300

    Create Bronze layer tables in ddl_bronze.sql
    This SQL script creates multiple tables for the Bronze layer, including customer information, product information, sales details, and ERP-related tables.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,67 @@
+/*
+Create Bronze Tables
+*/
+
+--CREATE TABLES FOR BRONZE LAYER
+
+CREATE TABLE bronze.crm_cust_info
+(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+)
+
+
+CREATE TABLE bronze.crm_prd_info
+(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE
+); 
+
+CREATE TABLE bronze.crm_sales_details
+(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+) 
+
+CREATE TABLE bronze.erp_CUST_AZ12
+(
+CID NVARCHAR(50),
+BDATE DATE,
+GEN NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_LOC_A101
+(
+CID NVARCHAR(50),
+CNTRY NVARCHAR(50)
+);
+
+
+/* IF OBJECT_ID('bronze.erp_PX_CAT_G1V2','U') IS NOT NULL
+	DROP TABLE bronze.erp_PX_CAT_G1V2 */
+
+CREATE TABLE bronze.erp_PX_CAT_G1V2
+(
+ID NVARCHAR(25),
+CAT NVARCHAR(50),
+SUBCAT NVARCHAR(50),
+MAINTENANCE NVARCHAR(10)
+);
+
